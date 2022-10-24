e4c1bb4432ff5b09e3c327378b0f6452322895ec
Juan Diego RR <jrojasr.1998@gmail.com>
2022-10-23 22:32:44 -0600

    Semana5 inicio
    Se inicia la semana5

---
+++ js/semana5.js
@@ -0,0 +1,18 @@
+/**
+Elaborar los siguientes ejercicios con lo aprendido en clases.
+1. Realizar una agenda telefónica de contactos.
+    a. Un contacto está definido por un nombre y un teléfono. Un contacto es igual a otro cuando sus nombres son iguales.
+    b. Una agenda de contactos está formada por un conjunto de contactos.
+    c. Se podrá crear de dos formas, indicándole nosotros el tamaño o con un tamaño por defecto (10).
+    d. Los métodos de la agenda serán los siguientes:
+        i. aniadirContacto(Contacto c): Añade un contacto a la agenda,sino se pueden meter más a la agenda se indicara por pantalla. No se puedenmeter contactos que existan, es decir, no podemos duplicar nombres, aunquetengan distinto teléfono.
+        ii. existeContacto(Conctacto c): indica si el contacto pasado existe o no.
+        iii. listarContactos():Lista toda la agenda.
+        iv. buscaContacto(nombre): busca un contacto por su nombre y muestra su teléfono.
+        v. eliminarContacto(Contacto c): elimina el contacto de la agenda, indica si se ha eliminado o no porpantalla.
+        vi. agendaLlena(): indicasi la agenda está llena.
+        vii. huecosLibres():indica cuantos contactos más podemos meter.
+    e. Crea interfaz gráfica.
+ */
+
+    "use strict";
